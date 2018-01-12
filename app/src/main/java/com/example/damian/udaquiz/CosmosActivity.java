7fa600d2b1ab285b1cb625d13beb0f595758e389
Damian <damian.suwala@gmail.com>
2018-01-12 22:16:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/damian/udaquiz/CosmosActivity.java
@@ -0,0 +1,13 @@
+package com.example.damian.udaquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CosmosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cosmos);
+    }
+}
---
+++ app/src/main/java/com/example/damian/udaquiz/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.damian.udaquiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button cosmos_btn = (Button) findViewById(R.id.cosmos_btn);
+
+        OnClickListener onClickListener = new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), CosmosActivity.class);
+                startActivity(intent);
+            }
+        };
+        cosmos_btn.setOnClickListener(onClickListener);
+
+
+
+    }
+
+}
